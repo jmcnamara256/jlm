2a6f6c0b54576e9b2fe9af00b8ddd2ac0b858ebd
Lucien Morey <lucienp@live.com.au>
2019-08-07 14:53:10 +1000

    added my pin functions from mx1

--- jlmio.c
+++ jlmio.c
@@ -20,3 +20,58 @@ void adc_setup() {
     ADMUX = 0b01000000;         // set the voltage reference to Vcc +5v
     ADCSRA = 0b10000111;        // turn on ADC, disable auto trigger, disable interrupts, set prescale
 }
+
+//Set pinmode for digital pins
+void pinMode(volatile uint8_t* pin, uint8_t position, uint8_t direction){
+    if (direction == INPUT){
+        *pin &= ~(1<<position);
+
+    } else if(direction == OUTPUT){
+        *pin |= (1<<position);
+
+    }
+}
+
+//read digital pin values
+int readPin(volatile uint8_t* pin, uint8_t position){
+    uint8_t output;
+
+    output = (~(*pin) & 1<<position);
+    return output;
+}
+
+//read write value to digital pin
+void writePin(volatile uint8_t* pin, uint8_t position,uint8_t value){
+    if(value == HIGH){
+        *pin |= (1<<position);
+        
+    }else if(value==LOW){
+        *pin &= ~(1<<position);
+        
+    }
+}
+
+//map values between a range
+float map(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// restrict values between a range of values
+int constrain(float x, int lowerbound, int upperbound){
+    if (x > upperbound){
+        x = upperbound;
+    } else if(x < lowerbound){
+        x = lowerbound;
+    }
+    return x;
+}
+
+//check sign of value
+int sign(int x){
+    if (x < 0){
+        return 0;
+    } else{
+        return 1;
+    }
+}
+
